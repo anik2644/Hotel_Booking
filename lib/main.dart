37ae10a787a8d2e5bc83484d48699ef9386b8618
anik11556 <abdullahal-2019317827@cs.du.ac.bd>
2022-07-31 11:32:17 +0600

    anik

---
+++ lib/main.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  get drawer => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          // backgroundColor: Colors.blue[300],
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+
+                automaticallyImplyLeading: true,
+                backgroundColor: Colors.black,
+                //floating: true,
+               // pinned: true,
+               // snap: false,
+                centerTitle: true,
+                title: const Text('Hotel Booking'),
+               actions: [
+                  IconButton(
+                    icon: const Icon(Icons.favorite),
+                    onPressed: () {},
+                  ),
+                ],
+                bottom: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.black,
+                  title: Container(
+                    width: double.infinity,
+                    height: 40,
+                    color: Colors.white,
+                    child: const Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search for something',
+                          prefixIcon: Icon(Icons.search),
+                          // suffixIcon: Icon(Icons.favorite)
+                        ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+            ],
+          ),
+
+/*
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Center(child: Text("Hotel Booking")),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {},
+              )
+            ],
+          ),
+*/
+
+          drawer: Drawer(
+            backgroundColor: Colors.black,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.white),
+                  accountName: Text(
+                    "MHD Mahmud Anik",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  accountEmail: Text("anik11556@gmail.com",
+                      style: TextStyle(color: Colors.black)),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    child: Text(
+                      "M",
+                      style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.black,
+                          backgroundColor: Colors.grey),
+                    ),
+                  ),
+                ),
+/*
+                ListTile(
+
+                  leading: Icon(Icons.home,color: Colors.white,),
+                  title: Text("Home",style: TextStyle(color: Colors.white),),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                */
+
+                ListTile(
+                  leading: Icon(Icons.settings, color: Colors.white),
+                  title:
+                      Text("Settings", style: TextStyle(color: Colors.white)),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.share,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "Share",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.contacts, color: Colors.white),
+                  title:
+                      Text("Contact Us", style: TextStyle(color: Colors.white)),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.flag_rounded,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "Terms & Condition",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "Logout",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.black,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.white,
+            // currentIndex: 2,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat),
+                label: 'Chat',
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_basket_sharp),
+                label: 'Offers',
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history),
+                label: 'History',
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Account',
+                backgroundColor: Colors.black,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
