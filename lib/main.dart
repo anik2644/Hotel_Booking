0f3450cdc8a08002733750571b7894bd85343f18
anik11556 <abdullahal-2019317827@cs.du.ac.bd>
2022-10-13 17:35:56 +0600

    "hotel list added "

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,52 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+
+  Container MyArticles(String imageVal, String hotelname,
+      String hotellocation) {
+    return Container(
+      width: 160.0,
+      child: Card(
+        child: Wrap(
+          children: <Widget>[
+            Image.network(imageVal),
+            ListTile(
+              title: Text(hotelname),
+              subtitle: Text(hotellocation),
+
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+
   get drawer => null;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           // backgroundColor: Colors.blue[300],
+          //appBar: ,
           body: CustomScrollView(
             slivers: [
+
+
               SliverAppBar(
 
                 automaticallyImplyLeading: true,
                 backgroundColor: Colors.black,
                 //floating: true,
-               // pinned: true,
-               // snap: false,
+                // pinned: true,
+                // snap: false,
                 centerTitle: true,
                 title: const Text('Hotel Booking'),
-               actions: [
+                actions: [
                   IconButton(
                     icon: const Icon(Icons.favorite),
                     onPressed: () {},
@@ -45,14 +70,42 @@ class MyApp extends StatelessWidget {
                           prefixIcon: Icon(Icons.search),
                           // suffixIcon: Icon(Icons.favorite)
                         ),
-                        ),
                       ),
                     ),
                   ),
                 ),
+              ),
 
-            ],
-          ),
+
+              SliverList(delegate: SliverChildListDelegate([
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20.0),
+                  height: 900 ,
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[
+                      MyArticles(
+                          "https://media.istockphoto.com/photos/luxury-resort-picture-id104731717?k=20&m=104731717&s=612x612&w=0&h=40INtJRzhmU1O4Rj24zdY8vj4aGsWpPaEfojaVQ8xBo=",
+                          "Hotel Grand Mahal", "Srinagar,India"),
+                      MyArticles(
+                          "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/07/1hotelsouthbeach.png",
+                          "Skylight Homez", "Singapore"),
+                      MyArticles(
+                          "https://img1.10bestmedia.com/Images/Photos/379999/Main-Pool-1_54_990x660.jpg",
+                          "The Royal Mount", "Dubai"),
+                      MyArticles(
+                          "https://imageio.forbes.com/specials-images/imageserve/5ec567daf2098c0006c6036e/Kimpton-Shanghai-Hotel/960x0.jpg?format=jpg&width=960",
+                          "Hotel Niharika", "Bangladesh"),
+                      MyArticles(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTjGpks3M8iiSaW-NvwJll2LTZQEkyfAj8OUV5_Jjs32LlQHCC2HbBfGBntZgMcfRRzDc&usqp=CAU",
+                          "Hotel Radisson", "Bangladesh"),
+                    ],
+                  ),),
+
+
+              ],
+              ),),
+            ],),
 
 /*
           appBar: AppBar(
@@ -105,7 +158,7 @@ class MyApp extends StatelessWidget {
                 ListTile(
                   leading: Icon(Icons.settings, color: Colors.white),
                   title:
-                      Text("Settings", style: TextStyle(color: Colors.white)),
+                  Text("Settings", style: TextStyle(color: Colors.white)),
                   onTap: () {
                     Navigator.pop(context);
                   },
@@ -126,7 +179,7 @@ class MyApp extends StatelessWidget {
                 ListTile(
                   leading: Icon(Icons.contacts, color: Colors.white),
                   title:
-                      Text("Contact Us", style: TextStyle(color: Colors.white)),
+                  Text("Contact Us", style: TextStyle(color: Colors.white)),
                   onTap: () {
                     Navigator.pop(context);
                   },
@@ -195,3 +248,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
